5bb1577eed5cefcbb871184411b0d9ff400cd2bf
Enrico Zini <enrico@enricozini.org>
2016-11-09 16:48:17 +0100

    Added some ArkiQuery tests

--- arki/runtime/arkiquery-test.cc
+++ arki/runtime/arkiquery-test.cc
@@ -1,6 +1,6 @@
-#include "config.h"
 #include "arkiquery.h"
 #include "arki/dataset/tests.h"
+#include "arki/metadata/collection.h"
 
 using namespace std;
 using namespace arki::tests;
@@ -10,9 +10,27 @@ using namespace arki::utils;
 
 namespace {
 
-class Tests : public TestCase
+struct Fixture : public DatasetTest {
+    using DatasetTest::DatasetTest;
+
+    void test_setup()
+    {
+        std::string config = R"(
+            type = ondisk2
+            step = daily
+            index = origin, reftime
+            unique = reftime, origin, product, level, timerange, area
+        )";
+        DatasetTest::test_setup(config);
+
+        testdata::GRIBData fixture;
+        wassert(import_all(fixture));
+    }
+};
+
+class Tests : public FixtureTestCase<Fixture>
 {
-    using TestCase::TestCase;
+    using FixtureTestCase::FixtureTestCase;
 
     void register_tests() override;
 } test("arki_runtime_arkiquery");
@@ -20,7 +38,46 @@ class Tests : public TestCase
 void Tests::register_tests() {
 
 // Export only binary metadata (the default)
-add_method("empty", [] {
+add_method("simple", [](Fixture& f) {
+    ArkiQuery query;
+    dataset::Reader::readConfig("testds", query.input_info);
+    query.set_query("");
+    query.set_output("test.md");
+    query.main();
+
+    wassert(actual_file("test.md").startswith("MD"));
+    metadata::Collection mdc;
+    mdc.read_from_file("test.md");
+    wassert(actual(mdc.size()) == 3u);
+});
+
+// Export only data
+add_method("simple", [](Fixture& f) {
+    ArkiQuery query;
+    dataset::Reader::readConfig("testds", query.input_info);
+    query.set_query("");
+    query.pmaker.data_only = true;
+    query.set_output("test.grib");
+    query.main();
+
+    wassert(actual_file("test.grib").startswith("GRIB"));
+    metadata::Collection mdc("test.grib");
+    wassert(actual(mdc.size()) == 3u);
+});
+
+// Postprocess
+add_method("postprocess", [](Fixture& f) {
+    ArkiQuery query;
+    dataset::Reader::readConfig("testds", query.input_info);
+    query.set_query("");
+    query.pmaker.postprocess = "checkfiles";
+    setenv("ARKI_POSTPROC_FILES", "/dev/null", 1);
+    query.set_output("test.out");
+    query.main();
+
+    wassert(actual(sys::read_file("test.out")) == "/dev/null\n");
+
+    unsetenv("ARKI_POSTPROC_FILES");
 });
 
 }
