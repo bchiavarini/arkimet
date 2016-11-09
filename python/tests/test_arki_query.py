e252e8e1f408e88dbf5eb41c7bb5bb2e29c27c6e
Enrico Zini <enrico@enricozini.org>
2016-11-09 18:47:09 +0100

    Test running postprocessors from python

--- python/tests/test_arki_query.py
+++ python/tests/test_arki_query.py
@@ -39,33 +39,15 @@ class TestArkiQuery(unittest.TestCase):
             self.assertEqual(data[:4], b"GRIB")
             self.assertEqual(data[-4:], b"7777")
 
+    def test_postprocess(self):
+        arki.arki_query(
+            query="",
+            input=self.input_cfg,
+            output="test.out",
+            postprocess="checkfiles",
+            postproc_data="/dev/null"
+        )
 
-#// Export only data
-#add_method("simple", [](Fixture& f) {
-#    ArkiQuery query;
-#    dataset::Reader::readConfig("testds", query.input_info);
-#    query.set_query("");
-#    query.pmaker.data_only = true;
-#    query.set_output("test.grib");
-#    query.main();
-#
-#    wassert(actual_file("test.grib").startswith("GRIB"));
-#    metadata::Collection mdc("test.grib");
-#    wassert(actual(mdc.size()) == 3u);
-#});
-#
-#// Postprocess
-#add_method("postprocess", [](Fixture& f) {
-#    ArkiQuery query;
-#    dataset::Reader::readConfig("testds", query.input_info);
-#    query.set_query("");
-#    query.pmaker.postprocess = "checkfiles";
-#    setenv("ARKI_POSTPROC_FILES", "/dev/null", 1);
-#    query.set_output("test.out");
-#    query.main();
-#
-#    wassert(actual(sys::read_file("test.out")) == "/dev/null\n");
-#
-#    unsetenv("ARKI_POSTPROC_FILES");
-#});
-#
+        with open("test.out", "rt") as fd:
+            data = fd.read()
+            self.assertEquals(data, "/dev/null\n")
